0516269bdf4bbc8ba8a2d1e4d229f20b9b018235
Anamcoollzz <hairulanam21@gmail.com>
2026-03-27 09:34:31 +0700

    Add UserSeeder to seed user data and update DatabaseSeeder to call it

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's user data.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => 'password',
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
